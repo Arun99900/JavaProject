8a717bc113b5a62857cb2dd3ea771d69e77fccd0
Arun <arunpatro973@gmail.com>
2022-07-05 09:05:56 +0530

    "EmployeeAttendance"

--- EmployeeWageCalculation/src/EmployeeWageCal/CheckEmployeeAttendance.java
+++ EmployeeWageCalculation/src/EmployeeWageCal/CheckEmployeeAttendance.java
@@ -1,10 +1,9 @@
 package EmployeeWageCal;
-
 public class CheckEmployeeAttendance {
 
 	    public static void main(String[] args) {
 		int IS_FULL_TIME = 1;
-		//Check Whether Employee is Present or Not
+		//Checking Whether Employee is Present or Not
 		double EmpCheck=Math.floor(Math.random()*10)% 2;
 		if(EmpCheck==IS_FULL_TIME)
 				System.out.println("Employee is Present");
